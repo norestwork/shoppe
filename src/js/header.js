4aead0f0403e937a9aec24c1a59935e7b1d7f460
NOREST <norestwork@gmail.com>
2024-03-07 18:16:09 +0500

    Header adaptive complete

---
+++ src/js/header.js
@@ -0,0 +1,28 @@
+$('.header__nav-icon-search').on('click', () => {
+  $('.header__bottom-form').slideToggle(200);
+})
+
+$('.header__nav-icon-burger').on('click', () => {
+  $('body,html').animate({scrollTop: 0}, 50);
+  $('.header__nav-bottom').slideToggle(100);
+  $('body').toggleClass('body--hidden');
+  $('.header__nav-icon-burger').toggleClass('cross');
+
+  burgerIcon();
+})
+
+$('.header__nav-bottom').on('click', () => {
+  $('.header__nav-bottom').slideToggle(100);
+  $('body').toggleClass('body--hidden');
+  $('.header__nav-icon-burger').toggleClass('cross');
+
+  burgerIcon();
+})
+
+const burgerIcon = () => {
+  if(!$('.header__nav-icon-burger').hasClass('cross')) {
+    $('.header__nav-icon-burger button').html('<img src="./../img/icons/burger-icon.svg" alt="Burger Icon">')
+  } else {
+    $('.header__nav-icon-burger button').html('<img src="./../img/icons/cross-icon.svg" alt="Cross Icon">')
+  }
+}
