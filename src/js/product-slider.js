3d383d9aa55dfa546a70cc05f71d7101c6f85a15
NOREST <norestwork@gmail.com>
2024-03-28 17:59:53 +0500

    Product slider progress bar complete

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -4,4 +4,13 @@ const main = new Splide('.product__slider-main', {
   arrows: false,
 });
 
+const bar = main.root.querySelector('.my-carousel-progress-bar');
+
+main.on( 'mounted move', () => {
+  let end  = main.Components.Controller.getEnd() + 1;
+  let rate = Math.min( ( main.index + 1 ) / end, 1 );
+  bar.style.width = String( 100 * rate ) + '%';
+} );
+
+
 main.mount();
